bc54bf80937599cc2431ce55d38da8da527f5ecd
dominikduda <gospodorz@gmail.com>
2019-03-07 22:26:43 +0100

    Return response not its body

--- lib/bitmex/http/private.rb
+++ lib/bitmex/http/private.rb
@@ -9,46 +9,46 @@ module Bitmex
 
         # @see https://www.bitmex.com/api/explorer/#!/Execution/Execution_get
         def execution(options = {})
-          @connection.get('/api/v1/execution', options).body
+          @connection.get('/api/v1/execution', options)
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Execution/Execution_getTradeHistory
         def execution_trade_history(options = {})
-          @connection.get('/api/v1/execution/tradeHistory', options).body
+          @connection.get('/api/v1/execution/tradeHistory', options)
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Leaderboard/Leaderboard_getName
         def leaderboard_name
-          @connection.get('/api/v1/leaderboard/name').body
+          @connection.get('/api/v1/leaderboard/name')
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Order/Order_getOrders
         def order(options = {})
-          @connection.get('/api/v1/order', options).body
+          @connection.get('/api/v1/order', options)
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Order/Order_new
         def create_order(symbol, order_qty, options = {})
-          @connection.post('/api/v1/order', { symbol: symbol, orderQty: order_qty }.merge(options)).body
+          @connection.post('/api/v1/order', { symbol: symbol, orderQty: order_qty }.merge(options))
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Order/Order_cancel
         def cancel_order(id, cid)
-          @connection.delete('/api/v1/order', {orderID: id, clOrdID: cid }).body
+          @connection.delete('/api/v1/order', {orderID: id, clOrdID: cid })
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Order/Order_cancelAll
         def cancel_all_order
-          @connection.delete('/api/v1/order/all').body
+          @connection.delete('/api/v1/order/all')
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Position/Position_get
         def position(options = {})
-          @connection.get('/api/v1/position', options).body
+          @connection.get('/api/v1/position', options)
         end
 
         def position_isolate(symbol, enabled)
-          @connection.post('/api/v1/position/isolate', { symbol: symbol, enabled: enabled }).body
+          @connection.post('/api/v1/position/isolate', { symbol: symbol, enabled: enabled })
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Order/Order_closePosition
@@ -57,41 +57,41 @@ module Bitmex
             symbol: symbol,
             price: price
           }.delete_if { |_, v| v.nil? }
-          @connection.post('/api/v1/order/closePosition', options).body
+          @connection.post('/api/v1/order/closePosition', options)
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Trade/Trade_getBucketed
         def trade_bucketed(bin_size = '1m', options = {})
-          @connection.get('/api/v1/trade/bucketed', { binSize: bin_size }.merge(options)).body
+          @connection.get('/api/v1/trade/bucketed', { binSize: bin_size }.merge(options))
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/User/User_get
         def user
-          @connection.get('/api/v1/user').body
+          @connection.get('/api/v1/user')
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/User/User_getAffiliateStatus
         def user_affiliate_status
-          @connection.get('/api/v1/user/affiliateStatus').body
+          @connection.get('/api/v1/user/affiliateStatus')
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/User/User_getWallet
         def user_wallet(currency = 'XBt')
-          @connection.get('/api/v1/user/wallet', { currency: currency }).body
+          @connection.get('/api/v1/user/wallet', { currency: currency })
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/User/User_getWalletHistory
         def user_wallet_history(currency = 'XBt')
-          @connection.get('/api/v1/user/walletHistory', { currency: currency }).body
+          @connection.get('/api/v1/user/walletHistory', { currency: currency })
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/User/User_getWalletSummary
         def user_wallet_summary(currency = 'XBt')
-          @connection.get('/api/v1/user/walletSummary', { currency: currency }).body
+          @connection.get('/api/v1/user/walletSummary', { currency: currency })
         end
 
         def user_margin(currency = 'XBt')
-          @connection.get('/api/v1/user/margin', { currency: currency }).body
+          @connection.get('/api/v1/user/margin', { currency: currency })
         end
       end
     end
--- lib/bitmex/http/public.rb
+++ lib/bitmex/http/public.rb
@@ -9,72 +9,72 @@ module Bitmex
 
         # @see https://www.bitmex.com/api/explorer/#!/Announcement/Announcement_get
         def announcement(options = {})
-          @connection.get('/api/v1/announcement', options).body
+          @connection.get('/api/v1/announcement', options)
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Announcement/Announcement_getUrgent
         def announcement_urgent(options = {})
-          @connection.get('/api/v1/announcement/urgent', options).body
+          @connection.get('/api/v1/announcement/urgent', options)
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Funding/Funding_get
         def funding(options = {})
-          @connection.get('/api/v1/funding', options).body
+          @connection.get('/api/v1/funding', options)
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Instrument/Instrument_get
         def instrument(options = {})
-          @connection.get('/api/v1/instrument', options).body
+          @connection.get('/api/v1/instrument', options)
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Instrument/Instrument_getActive
         def instrument_active
-          @connection.get('/api/v1/instrument/active').body
+          @connection.get('/api/v1/instrument/active')
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Insurance/Insurance_get
         def insurance(options = {})
-          @connection.get('/api/v1/insurance', options).body
+          @connection.get('/api/v1/insurance', options)
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Leaderboard/Leaderboard_get
         def leaderboard(options = {})
-          @connection.get('/api/v1/leaderboard', options).body
+          @connection.get('/api/v1/leaderboard', options)
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Liquidation/Liquidation_get
         def liquidation(options = {})
-          @connection.get('/api/v1/liquidation', options).body
+          @connection.get('/api/v1/liquidation', options)
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/OrderBook/OrderBook_getL2
         def order_book_l2(symbol, depth = 25)
-          @connection.get('/api/v1/orderBook/L2', { symbol: symbol, depth: depth }).body
+          @connection.get('/api/v1/orderBook/L2', { symbol: symbol, depth: depth })
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Settlement/Settlement_get
         def settlement(options = {})
-          @connection.get('/api/v1/settlement', options).body
+          @connection.get('/api/v1/settlement', options)
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Stats/Stats_get
         def stats
-          @connection.get('/api/v1/stats').body
+          @connection.get('/api/v1/stats')
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Stats/Stats_history
         def stats_history
-          @connection.get('/api/v1/stats/history').body
+          @connection.get('/api/v1/stats/history')
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Stats/Stats_historyUSD
         def stats_history_usd
-          @connection.get('/api/v1/stats/historyUSD').body
+          @connection.get('/api/v1/stats/historyUSD')
         end
 
         # @see https://www.bitmex.com/api/explorer/#!/Trade/Trade_get
         def trade(options = {})
-          @connection.get('/api/v1/trade', options).body
+          @connection.get('/api/v1/trade', options)
         end
       end
     end
